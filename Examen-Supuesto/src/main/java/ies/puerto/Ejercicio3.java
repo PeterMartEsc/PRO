233add80b7347bc843d7f16ab01114835dbf7a85
PeterMartEsc <petermartesc@gmail.com>
2023-11-10 18:06:18 +0000

    Se actualiza el simulacro de exámen

--- Examen-Supuesto/src/main/java/ies/puerto/Ejercicio3.java
+++ Examen-Supuesto/src/main/java/ies/puerto/Ejercicio3.java
@@ -2,6 +2,11 @@ package ies.puerto;
 
 import java.util.Scanner;
 
+/**
+ * Programa para determinar la evaluación de un alumno a partir de una nota
+ * @author PeterMartEsc
+ */
+
 public class Ejercicio3 {
 
     public static void main (String[]args){
@@ -11,25 +16,31 @@ public class Ejercicio3 {
         System.out.print("Ingrese su nota: ");
         float nota = scanner.nextFloat();
 
+        System.out.println("Su evaluacion calculada por If/Else es: " +calcularNotaIfElse(nota) );
+
+    }
+
+    public static String calcularNotaIfElse (float nota){
+
         String evaluacion = "Por definir";
 
-        if (nota >= 0 && nota <= 4.99) {
+        if (nota >= 0f && nota <= 4.99f) {
             evaluacion = "Suspenso";
-        } else if (nota >=5 && nota <=5.99) {
+        } else if (nota >=5f && nota <=5.99f) {
             evaluacion = "Aprobado";
-        } else if (nota >= 6 && nota <= 6.99) {
+        } else if (nota >= 6f && nota <= 6.99f) {
             evaluacion = "Bien";
-        } else if (nota >= 7 && nota <= 8.99) {
+        } else if (nota >= 7f && nota <= 8.99f) {
             evaluacion = "Notable";
-        } else if (nota >= 9 && nota <= 9.99) {
+        } else if (nota >= 9f && nota <= 9.99f) {
             evaluacion = "Sobresaliente";
-        } else if (nota == 10) {
+        } else if (nota == 10f) {
             evaluacion = "Matrícula";
         } else {
             System.out.println("Introduzca un valor entre 0 y 10");
         }
 
-        System.out.println("Tu evaluacion: " + evaluacion);
+        return evaluacion;
     }
 
 
