7d1f73ef1aded292acb6d2458e5578d0dc81d6de
PeterMartEsc <petermartesc@gmail.com>
2023-12-01 18:41:46 +0000

    Se ha añadido un nuevo ejercicio

---
+++ Unidad-3/ClaseString/src/main/java/ies/puerto/Ejercicio1.java
@@ -0,0 +1,14 @@
+package ies.puerto;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Ejercicio1 {
+    public static void main(String[] args) {
+
+        String cadena = "Feliz Navidad";
+        int length = cadena.length();
+
+        System.out.println("La longitud de la cadena es: " +length);
+
+    }
+}
---
+++ Unidad-3/ClaseString/src/main/java/ies/puerto/Ejercicio2.java
@@ -0,0 +1,11 @@
+package ies.puerto;
+
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+
+        String cadena1 = "Hola, mundo!";
+        String cadena2 = "Feliz Navidad";
+        boolean isEqual = cadena1.equals(cadena2);
+    }
+}
