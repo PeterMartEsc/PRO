ee48c0bb50886df799a2d86c4d3e84425e5508f5
PeterMartEsc <petermartesc@gmail.com>
2024-04-01 14:39:24 +0100

    Se actualiza

---
+++ Unidad-5/Ficheros/PersonajesMarvel/src/main/java/es/ies/puerto/file/Apuntes.java
@@ -0,0 +1,20 @@
+package es.ies.puerto.file;
+
+public class Apuntes {
+
+    // Json
+
+    // String json = new String(Files.readAllBytes(Paths.get(path)));
+    // Type listType = new TypeToken<List<Personaje>>(){}.getType();
+    // personajes = new Gson().fromJson(json, listType);
+
+    // File writer = new FileWriter(path);
+    // new GsonBuilder().setPrettyPrinting().create().toJson(personajes, writer);
+
+    // Xml
+
+    // Persister serializer = new Persister();
+    //
+    // File file = new File(path);
+    // PersonajeList personajeList = new
+}
