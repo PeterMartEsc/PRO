367127de479882e8a9ee3fc71fb272d7cada94da
PeterMartEsc <petermartesc@gmail.com>
2024-04-05 18:43:42 +0100

    Se actualiza el trabajo en clase

--- Unidad-7/SQLite/gestion-usuarios-BBDD/src/main/java/es/ies/puerto/config/AppConfig.java
+++ Unidad-7/SQLite/gestion-usuarios-BBDD/src/main/java/es/ies/puerto/config/AppConfig.java
@@ -1,8 +1,21 @@
 package es.ies.puerto.config;
 
+import es.ies.puerto.exception.UsuarioException;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class AppConfig {
+    String urlBd;
+    public AppConfig() throws UsuarioException {
+        Properties properties = new Properties();
+        try (FileInputStream fis = new FileInputStream("config.properties")) {
+            properties.load(fis);
 
-    public AppConfig(){
-
+            urlBd = (String) properties.get("urlBd");
+        } catch (IOException e) {
+            throw new UsuarioException(e.getMessage(), e);
+        }
     }
 }
